06988b25c086b5cf94a010b7df615801c757fbbe
maksgr <max.hrybun@n-development.com>
2020-10-09 10:58:33 +0300

    add technical-training-13.0-01-models

--- my_addons/custom_mod/models/sale_order_line.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    express_shipping = fields.Boolean("Express Shipping", default=False)
