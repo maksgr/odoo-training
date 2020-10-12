37afed7b0ca5296df10ad97b11a92f4b8394f238
makshrybun <goodDay2019>
2020-10-12 12:08:21 +0300

    task-training:[DEV] added related fields

--- my_addons/library/models/rental.py
+++ my_addons/library/models/rental.py
@@ -13,4 +13,8 @@ class Rental(models.Model):
     book_id = fields.Many2one('library.book', 'Book')
     rental_date = fields.Date(string='Rental Date')
     return_date = fields.Date(string='Return Date')
-
+    customer_address = fields.Text('Customer Address', related='customer_id.address')
+    customer_email = fields.Char('Customer Email', related='customer_id.email')
+    book_author_ids = fields.Many2many(related='book_id.author_ids')
+    book_edition_date = fields.Date(string='Edition Date', related='book_id.edition_date')
+    book_publisher_id = fields.Many2one(related='book_id.publisher_id')
